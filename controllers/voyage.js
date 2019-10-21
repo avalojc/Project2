b97dd04ca94d9884233f68d369863f4c023986b5
JC <avalojc@gmail.com>
2019-10-21 09:50:25 -0400

    created getAll and create, tested functionality using res json. updated name of api to voyageApi

--- controllers/voyage.js
+++ controllers/voyage.js
@@ -1,12 +1,30 @@
 const express = require('express')
 
-const templateApi = require('../models/voyage.js')
+const voyageApi = require('../models/voyage.js')
 
 const voyageRouter = express.Router()
 
 // templateRouter.get('/', (req, res) => {
 //   res.send(templateApi.getHelloWorldString())
 // })
+
+//getAll
+voyageRouter.get('/voyage', (req, res) => {
+  voyageApi.getAllVoyage()
+    .then((allVoyage) => {
+      res.json(allVoyage)
+    })
+})
+
+//create
+voyageRouter.post('/voyage', (req, res) => {
+  voyageApi.addNewVoyage(req.body)
+    .then((createVoyage) => {
+      res.json(createVoyage)
+    })
+})
+
+
 
 module.exports = {
   voyageRouter
