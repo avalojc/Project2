dad42396b1a5b7e261c3572c38a711553fc08e1b
JC <avalojc@gmail.com>
2019-10-21 10:13:51 -0400

    added other functionalities, and made note for create issue where id is the only item posted

--- controllers/voyage.js
+++ controllers/voyage.js
@@ -4,10 +4,6 @@ const voyageApi = require('../models/voyage.js')
 
 const voyageRouter = express.Router()
 
-// templateRouter.get('/', (req, res) => {
-//   res.send(templateApi.getHelloWorldString())
-// })
-
 //getAll
 voyageRouter.get('/voyage', (req, res) => {
   voyageApi.getAllVoyage()
@@ -22,15 +18,27 @@ voyageRouter.get('/voyage/:voyageId', (req, res) => {
       res.json(singleVoyage)
     })
 })
-
-//create
+//update
+voyageRouter.put('/voyage/:voyageId', (req, res) => {
+  voyageApi.editVoyage(req.params.voyageId, req.body)
+    .then((editVoyage) => {
+      res.json(editVoyage)
+    })
+})
+//create ****************for some reason it is not changing the inner workings just making an id and ish. idk.
 voyageRouter.post('/voyage', (req, res) => {
   voyageApi.addNewVoyage(req.body)
     .then((createVoyage) => {
       res.json(createVoyage)
     })
 })
-
+//delete
+voyageRouter.delete('/voyage/:voyageId', (req, res) => {
+  voyageApi.deleteVoyage(req.params.voyageId)
+    .then((deleteVoyage) => {
+      res.json(deleteVoyage)
+    })
+})
 
 
 module.exports = {
