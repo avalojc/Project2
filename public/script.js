8329acbc79f1c4a3fff04db604d33f320e9c8d01
JC <avalojc@gmail.com>
2019-10-24 13:00:30 -0400

    mad it so that html shows up when an bject is droped in the box

--- public/script.js
+++ public/script.js
@@ -1,8 +1,16 @@
 console.log('Hello world')
 
-$( function() {
+
+$( () => {
     $( ".infoBlock" ).draggable();
-} );
-
-
+    // let a = document.getElementsByClassName("infoBlock")
+    $( ".row" ).droppable({
+      drop: function( event, ui ) {
+        $( this )
+          .addClass( "ui-state-highlight" )
+          .find( ".adder" )
+            .html("this is what i want to show up");
+      }
+    });
+  } );
   console.log('Ran Bruh')
