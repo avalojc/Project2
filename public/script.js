632c9054dc57aa1183d2876d1749fc6adc56d0e2
JC <avalojc@gmail.com>
2019-10-24 13:25:46 -0400

    made a var that could be put into DATA

--- public/script.js
+++ public/script.js
@@ -4,12 +4,13 @@ console.log('Hello world')
 $( () => {
     $( ".infoBlock" ).draggable();
     // let a = document.getElementsByClassName("infoBlock")
+    let a = "xxx"
     $( ".row" ).droppable({
       drop: function( event, ui ) {
         $( this )
           .addClass( "ui-state-highlight" )
-          .find( ".adder" )
-            .html("this is what i want to show up");
+          .find( ".data" )
+            .html(a);
       }
     });
   } );
